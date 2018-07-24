8dd6c0c3a250aeb88dccd45a121e6e2961d1a825
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-07-24 19:43:45 -0300

    Removed useless step

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -23,20 +23,9 @@ def step_impl(context, screen):
 
 
 @given(u'que vou para a tela {screen}')
+@given(u'vou para a tela {screen}')
 def step_impl(context, screen):
     context.config.driver.open_screen(screen)
-
-
-@given(u'seleciono a select {name} com o texto {texto}')
-def step_impl(context, name, texto):
-    if not texto == "<ignora>":
-        select = Select(context.config.driver.find_element_by_name(name))
-        select.select_by_visible_text(texto)
-        selected_option = select.first_selected_option
-        set_value = selected_option.text
-        assert set_value == texto
-    else:
-        pass
 
 
 # @given(u'preencho o campo de nome {campo} com o valor {valor} valor esperado {valor_esperado}')
