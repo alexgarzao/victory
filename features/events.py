27b4b0108a4846a0e8336c3eb66f9d64512eadf1
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-07-30 16:55:52 -0300

    Now events could have parameters (like {name}, for example)

--- features/events.py
+++ features/events.py
@@ -1,3 +1,6 @@
+from parse import *
+
+
 class Events:
     def __init__(self):
         self.events = {}
@@ -10,12 +13,27 @@ class Events:
         event.append(event_step)
 
     def run(self, context, event_name):
-        assert event_name in self.events
-        if event_name not in self.events:
-            assert False
-            context.execute_steps("quando " + event_name)
-            return
-
-        event = self.events[event_name]
+        # TODO: retornar steps aos inves de executar. Assim nao precisa conhecer context.
+        event, parameters = self.__match_event(event_name)
+        assert event != None
+        steps_to_run = ''
         for step_event in event:
-            context.execute_steps("quando " + step_event)
+            step_event = self.__replace_parameters(step_event, parameters)
+            steps_to_run += step_event + '\n'
+
+        context.execute_steps(steps_to_run)
+
+    def __match_event(self, event):
+        for event_type in self.events.keys():
+            r = parse(event_type, event)
+            if r:
+                return self.events[event_type], r.named
+
+        return None
+
+    def __replace_parameters(self, step_event, parameters):
+        for parameter, value in parameters.items():
+            token_to_find = "{" + parameter + "}"
+            step_event = step_event.replace(token_to_find, value)
+
+        return step_event
