3d51bab3d7926857efc6592eff2e1a303c4efc50
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-09-03 16:44:36 -0300

    Script to run all samples

---
+++ run_samples.sh
@@ -0,0 +1,5 @@
+reset && \
+make FEATURES_PATH=samples/google-search && \
+make FEATURES_PATH=samples/page-object-pattern && \
+make FEATURES_PATH=samples/find-elements && \
+make FEATURES_PATH=samples/google-search-with-sqlite
