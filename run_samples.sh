3f75bfa826d83508d0642c6146f4e2299fc3cc2f
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-09-10 16:52:50 -0300

    Running in headless mode too (samples)

--- run_samples.sh
+++ run_samples.sh
@@ -2,4 +2,8 @@ reset && \
 beeweb run samples/google-search && \
 beeweb run samples/page-object-pattern && \
 beeweb run samples/find-elements && \
-beeweb run samples/google-search-with-sqlite
+beeweb run samples/google-search-with-sqlite && \
+beeweb run --headless samples/google-search && \
+beeweb run --headless samples/page-object-pattern && \
+beeweb run --headless samples/find-elements && \
+beeweb run --headless samples/google-search-with-sqlite
