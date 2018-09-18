fd75d7d420afd3e5f02dbdaba2c2596467fcb0a8
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-09-18 17:59:36 -0300

    FIX: TDD

--- tests/features/context.py
+++ tests/features/context.py
@@ -3,4 +3,8 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from ..custom_asserts import *
-from features import *
+from features.support.actions import *
+from features.support.files import *
+from features.support.mask import *
+from features.support.queries import *
+from features.support.sqlite import *
--- tests/web_features/context.py
+++ tests/web_features/context.py
@@ -3,5 +3,7 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 
 from ..custom_asserts import *
-from features import *
-from web_features import *
+from web_features.support.element import *
+from web_features.support.screen import *
+from web_features.support.screens import *
+from web_features.support.web_driver import *
