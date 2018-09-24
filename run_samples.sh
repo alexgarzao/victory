5c5a199c0ac298196fb6dc7f0ee4bc21e2218136
Alex S. Garzão <alex.garzao@unicred.com.br>
2018-09-24 20:57:57 -0300

    Reorganizing the samples to be compliant with the new scenarios dir layout

--- run_samples.sh
+++ run_samples.sh
@@ -6,6 +6,7 @@ victory run web samples/google-search-with-sqlite && \
 victory run web samples/frames && \
 victory run web samples/frames_2 && \
 victory run web samples/upload && \
+victory run web samples/download && \
 victory run web samples/windows && \
 victory run --headless web samples/google-search && \
 victory run --headless web samples/page-object-pattern && \
@@ -14,4 +15,11 @@ victory run --headless web samples/google-search-with-sqlite && \
 victory run --headless web samples/frames && \
 victory run --headless web samples/frames_2 && \
 victory run --headless web samples/upload && \
+victory run --headless web samples/download && \
 victory run --headless web samples/windows
+
+
+#google-search-more-scenarios
+#show-unused-definitions
+#testing-screenshots
+#using_tags
